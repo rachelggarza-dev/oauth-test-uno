5adecddc43f1eaf1a51797393f74ff9e7247e0d8
Rachel Garza <rachelg@unity3d.com>
2018-03-28 16:48:08 -0500

    adding the project stuff.

---
+++ Assets/Editor/NewEditModeTest1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewEditModeTest1 {
+
+	[Test]
+	public void NewEditModeTest1SimpleFailure() {
+		Assert.AreEqual (1, 0);
+	}
+	
+	[UnityTest]
+	public IEnumerator NewEditModeTest1WithEnumeratorFailure() {
+		Assert.AreEqual (1, 0);
+		yield return null;
+	}
+}
---
+++ Assets/NewPlayModeTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewPlayModeTest {
+
+	[Test]
+	public void NewPlayModeTestSimpleFailures() {
+		Assert.AreEqual (1, 0);
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator NewPlayModeTestWithEnumeratorFailures() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return false;
+	}
+}
